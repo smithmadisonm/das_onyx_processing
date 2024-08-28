a3bfaa458e940b4e788e00c6d6b873a768c73e0e
Nathan Figueroa <nathan.figueroa@whoi.edu>
2024-08-28 16:24:16 -0400

    feat: Enable for command line, misc bug fixes

--- process.py
+++ process.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 import h5py as h5
 from matplotlib import pyplot as plt
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 import xarray as xr
 import glob
 from scipy import signal
 from joblib import Parallel, delayed
 import time as tm
-from datetime import timedelta
+import argparse
 
 def datetime_range(start, end, delta):
     current = start
@@ -18,16 +18,18 @@ def datetime_range(start, end, delta):
 
 def load_h5_into_xr_chunk(base_path, time_target, time_chunk):
     #file names for all files including times within the 30 min chunk
-    files_target_chunk = [ glob.glob(onyx_path + '*' + datetime.strftime(time_temp,'%Y-%m-%d_%H.%M') +'*.h5') for time_temp in 
-           datetime_range(time_target-timedelta(minutes=1), time_target + timedelta(minutes=time_chunk), 
+    files_target_chunk = [glob.glob(base_path + 'decimator_' + datetime.strftime(time_temp,'%Y-%m-%d_%H.%M') +'*.h5') for time_temp in 
+           datetime_range(time_target, time_target + timedelta(minutes=time_chunk), 
            timedelta(minutes=1))]
+    
+    ds_DAS_chunk = None
+    attrs = None
+    
     for fi in files_target_chunk:
-        #fi = files_target_chunk[0]
         if not fi:
-            print("List is empty")
+            print(f"No files found for time: {time_target}")
             continue
-        else: #only continue if there is a fule 
-                
+        else:
             f = h5.File(fi[0],'r')
 
             #data and key parameters
@@ -37,7 +39,6 @@ def load_h5_into_xr_chunk(base_path, time_target, time_chunk):
 
             #create actual times, where file time is in microseconds from #
             file_timestr = fi[0].split('/')[-1][10:-7]
-            #print(file_timestr)
             file_datetime = datetime.strptime(file_timestr, '%Y-%m-%d_%H.%M.%S')
             f_seconds = f_data.shape[0]/f['Acquisition'].attrs['PulseRate'] #length of time, in seconds, of array
             dt_ms = 1000000/f['Acquisition'].attrs['PulseRate'] #time in ms between each ...at 250 Hz, 4000 micros between each timestep
@@ -65,20 +66,24 @@ def load_h5_into_xr_chunk(base_path, time_target, time_chunk):
             ds_DAS = xr.Dataset(data_vars=data_DAS, 
                             coords=coords)
 
-            if 'ds_DAS_chunk' in locals():
-                ds_DAS_chunk = xr.merge([ds_DAS_chunk,ds_DAS])
-            else:
+            if ds_DAS_chunk is None:
                 ds_DAS_chunk = ds_DAS
-    ds_DAS_chunk = ds_DAS_chunk.assign_attrs(attrs)
+            else:
+                ds_DAS_chunk = xr.merge([ds_DAS_chunk, ds_DAS])
     
+    if ds_DAS_chunk is None:
+        print(f"No data found for time chunk starting at {time_target}")
+        return None
+    
+    ds_DAS_chunk = ds_DAS_chunk.assign_attrs(attrs)
+
     #select exactly the 30 minutes from the full combined array
     fs = ds_DAS_chunk.attrs['PulseRate']
     ds_DAS_chunk = ds_DAS_chunk.sel(time=slice(time_target, time_target+timedelta(minutes=time_chunk)))
     if len(ds_DAS_chunk.time) < time_chunk*fs*60:
-        print('Stop, missing data: '+str(len(ds_DAS_chunk.time)) + ' should be ' + str(time_chunk*ds_DAS_chunk.attrs['PulseRate']*60)) 
+        print('Warning: Missing data: '+str(len(ds_DAS_chunk.time)) + ' should be ' + str(time_chunk*ds_DAS_chunk.attrs['PulseRate']*60))
 
     return ds_DAS_chunk
-
 
 def das_butterworth_decimate_xarray(ds_DAS_chunk, fs_target):
     
@@ -106,40 +111,54 @@ def das_butterworth_decimate_xarray(ds_DAS_chunk, fs_target):
         strain_deci_butter = strain_butter[::t_inc]
         ds_DAS_deci[:,i] = strain_deci_butter
 
-    #make xarray
+    attrs_deci = ds_DAS_chunk.attrs.copy()
+    attrs_deci['PulseRateDecimated'] = fs_target
+    attrs_deci['DecimationFilterType'] = 'butterworth'
 
-    attrs_deci = attrs
-    attrs_deci['PulseRateDecimated']=fs_target
-    attrs_deci['DecimationFilterType']='butterworth'
-
-    coords = {'time': (['time'], ds_DAS_chunk.time[0::t_inc]),
-                  'channels': (['channels'], channels)}
-
-    data_deci = {'strain':(['time','channels'], ds_DAS_deci, 
-                            {'units':'',
-                           'long_name':'decimated strain data'})}
-    strain_deci_butter_all = xr.Dataset(data_vars=data_deci,coords=coords,attrs=attrs_deci)
+    # Ensure time values are numpy array
+    time_values = ds_DAS_chunk.time.values[0::t_inc]
     
+    coords = {
+        'time': ('time', time_values),
+        'channels': ('channels', ds_DAS_chunk.channels.values)
+    }
+
+    data_deci = {
+        'strain': (['time','channels'], ds_DAS_deci, {'units':'', 'long_name':'decimated strain data'})
+    }
+    
+    strain_deci_butter_all = xr.Dataset(data_vars=data_deci, coords=coords, attrs=attrs_deci)
+
     return strain_deci_butter_all
 
 
+def process_data(onyx_path, start_time, end_time, time_chunk, fs_target, output_dir):
+    for di in datetime_range(start_time, end_time, timedelta(minutes=30)):
+        print(str(di)+', current time '+str(datetime.now()))
+        ds_DAS_chunk = load_h5_into_xr_chunk(onyx_path, di, time_chunk)
+        if ds_DAS_chunk is None:
+            print(f"Skipping time chunk starting at {di}")
+            continue
+        strain_deci_butter_all = das_butterworth_decimate_xarray(ds_DAS_chunk, fs_target)
 
-time_chunk = 30 #min files 
-fs_target = 5 #target frequency, Hz
+        output_file = f'{output_dir}Onyx_{di.strftime("%Y-%m-%d_%H.%M")}_{fs_target}hz.nc'
+        strain_deci_butter_all.to_netcdf(output_file)
 
-#files to read in for target time 
-onyx_path = '/Volumes/OnyxDASdata/FEB_DATA/'
+def main():
+    parser = argparse.ArgumentParser(description="Process DAS data and decimate it.")
+    parser.add_argument("onyx_path", type=str, help="Path to the Onyx DAS data")
+    parser.add_argument("start_time", type=str, help="Start time in format YYYY-MM-DD HH:MM")
+    parser.add_argument("end_time", type=str, help="End time in format YYYY-MM-DD HH:MM")
+    parser.add_argument("output_dir", type=str, help="Output directory for processed files")
+    parser.add_argument("--time_chunk", type=int, default=30, help="Time chunk in minutes")
+    parser.add_argument("--fs_target", type=int, default=5, help="Target frequency in Hz")
 
-#range of target times
-start_time = datetime(2023, 11, 23, 3, 0) #2023,11,22,0,0 having issues matching strain length to time length... 
-end_time = datetime(2023, 12, 18, 0, 0)
+    args = parser.parse_args()
 
-#directory for saving decimated
-dir_5hz = '/Users/msmith/Documents/DAS/MVCO/202311_MVCO/Onyx_DASdata_5hz/'
+    start_time = datetime.strptime(args.start_time, "%Y-%m-%d %H:%M")
+    end_time = datetime.strptime(args.end_time, "%Y-%m-%d %H:%M")
 
-for di in datetime_range(start_time, end_time, timedelta(minutes=30)):
-    print(str(di)+', current time '+str(datetime.now()))
-    ds_DAS_chunk = load_h5_into_xr_chunk(onyx_path, di, time_chunk)
-    strain_deci_butter_all = das_butterworth_decimate_xarray(ds_DAS_chunk,fs_target)
-    
-    strain_deci_butter_all.to_netcdf(dir_5hz+'Onyx_'+datetime.strftime(di,'%Y-%m-%d_%H.%M')+'_'+str(fs_target)+'hz.nc')
+    process_data(args.onyx_path, start_time, end_time, args.time_chunk, args.fs_target, args.output_dir)
+
+if __name__ == "__main__":
+    main()
